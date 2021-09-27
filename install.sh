b441624fef945b30148853b41360a79796802847
John Robinson <johnrobinsn@gmail.com>
2021-09-26 22:51:25 -0400

    Now getting fully qualified path to scripts

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-BASEDIR="$(dirname "$BASH_SOURCE")"
+ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+echo $ABSOLUTE_PATH
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo Install from $BASEDIR
 ln -sFi $BASEDIR/tmux/.tmux.conf ~/.tmux.conf
